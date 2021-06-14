3d2670bbfb7b048d11679cce62a39f687fa9ad58
katboi01 <katboiofficial@gmail.com>
2021-06-14 16:48:10 +0200

    texture replacer (WIP, useless)

--- KF3Translate/Program.cs
+++ KF3Translate/Program.cs
@@ -21,7 +21,7 @@ namespace KF3Translate
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Program p = new Program();
-            p.ReplaceScenarios();
+            p.ReplaceTextures();
         }
 
         private void ReplaceScenarios()
@@ -117,7 +117,94 @@ namespace KF3Translate
                 bundleInst.file.Close();
 
                 File.Delete(selectedFile + "_temp");
-                am.UnloadAll(); //delete this if something breaks
+            }
+        }
+
+        /// <summary>
+        /// Almost functional but currently useless, requires ready texture assets
+        /// </summary>
+        private void ReplaceTextures()
+        {
+            LoadDirectoryPath = Path.Combine(Environment.CurrentDirectory, @"Data\Textures\");
+            SaveDirectoryPath = Path.Combine(Environment.CurrentDirectory, @"Data\Test\");
+
+            am = new AssetsManager();
+            am.LoadClassPackage(Path.Combine(Environment.CurrentDirectory, @"Data\classdata.tpk"));
+
+            foreach (string dir in Directory.GetDirectories(LoadDirectoryPath))
+            {
+                if (!File.Exists(Path.Combine(SaveDirectoryPath, Path.GetFileName(dir)))) { continue; }
+
+                Console.WriteLine("Replacing file " + Path.GetFileName(dir));
+
+                List<string> fileNames = Directory.GetFiles(dir).Select(d => Path.GetFileNameWithoutExtension(d)).ToList();
+
+                //load file
+                string selectedFile = Path.Combine(SaveDirectoryPath, Path.GetFileName(dir));
+                BundleFileInstance bundleInst = am.LoadBundleFile(selectedFile, false);
+
+                //Decompress the file to memory (overwrites bundleInst)
+                bundleInst.file = Utilities.DecompressToMemory(bundleInst);
+
+                AssetsFileInstance inst = am.LoadAssetsFileFromBundle(bundleInst, 0);
+                am.LoadClassDatabaseFromPackage(inst.file.typeTree.unityVersion);
+
+                for (int i = 0; i < inst.table.assetFileInfoCount; i++)
+                {
+                    string naiveName = AssetHelper.GetAssetNameFastNaive(inst.table.file, inst.table.assetFileInfo[i]);
+                    if (fileNames.Contains(naiveName))
+                    {
+                        Console.WriteLine($"File {i}: {naiveName} ({inst.table.assetFileInfo[i].curFileType})");
+                        AssetFileInfoEx inf = inst.table.GetAssetInfo(naiveName);
+                        AssetTypeValueField baseField = am.GetTypeInstance(inst.file, inf).GetBaseField();
+
+                        //Load replacer file
+                        string selectedFile2 = Path.Combine(dir, naiveName);
+                        BundleFileInstance bundle2Inst = am.LoadBundleFile(selectedFile2, false);
+                        bundle2Inst.file = Utilities.DecompressToMemory(bundle2Inst);
+                        AssetsFileInstance inst2 = am.LoadAssetsFileFromBundle(bundle2Inst, 0);
+                        AssetFileInfoEx inf2 = inst2.table.GetAssetInfo("animalpicture_0001");
+
+                        //Utilities.PrintAssetBundleContent(inst);
+                        AssetTypeValueField baseField2 = am.GetTypeInstance(inst2.file, inf2).GetBaseField();
+
+                        //completely replaces texture asset's content, might not work
+                        baseField.SetChildrenList(baseField2.GetChildrenList());
+                        baseField.children[0].GetValue().Set(naiveName);
+
+                        //commit changes
+                        byte[] newAssetData;
+                        byte[] newGoBytes = baseField.WriteToByteArray();
+                        AssetsReplacerFromMemory repl = new AssetsReplacerFromMemory(0, inf.index, (int)inf.curFileType, 0xFFFF, newGoBytes);
+                        using (MemoryStream stream = new MemoryStream())
+                        using (AssetsFileWriter writer = new AssetsFileWriter(stream))
+                        {
+                            inst.file.Write(writer, 0, new List<AssetsReplacer>() { repl }, 0);
+                            newAssetData = stream.ToArray();
+                        }
+
+                        BundleReplacerFromMemory bunRepl = new BundleReplacerFromMemory(inst.name, null, true, newAssetData, -1);
+
+                        //write a modified file (temp)
+                        using (var stream = File.OpenWrite(selectedFile + "_temp"))
+                        using (var writer = new AssetsFileWriter(stream))
+                        {
+                            bundleInst.file.Write(writer, new List<BundleReplacer>() { bunRepl });
+                        }
+                        bundleInst.file.Close();
+
+                        //load the modified file for compression
+                        bundleInst = am.LoadBundleFile(selectedFile + "_temp");
+                        using (var stream = File.OpenWrite(selectedFile))
+                        using (var writer = new AssetsFileWriter(stream))
+                        {
+                            bundleInst.file.Pack(bundleInst.file.reader, writer, AssetBundleCompressionType.LZ4);
+                        }
+                        bundleInst.file.Close();
+
+                        File.Delete(selectedFile + "_temp");
+                    }
+                }
             }
         }
     }
